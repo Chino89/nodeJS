ad4586d4749ad70d0780355d4a238f3456117fc9
Emma <x.emmavivas@gmail.com>
2023-06-27 21:32:27 -0300

    users CRUD

--- src/models/user.js
+++ src/models/user.js
@@ -1,32 +1,40 @@
-const { DataTypes } = require("sequelize");
-const { sequelize } = require("../config/db-config");
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/db-config');
 
-const User = sequelize.define("Users", {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+const User = sequelize.define('Users', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      not: 'admin',
     },
-    firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
     },
-    lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true,
-        },
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  active: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  },
 });
 
 module.exports = User;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,13 @@
 const express = require('express');
-const router = express.Router()
 
+const router = express.Router();
+
+const { userController } = require('../controllers');
+
+router.post('/', userController.createAdmin);
+router.get('/all', userController.getAllUsers);
+router.get('/:userId', userController.getUser);
+router.post('/:userId', userController.updateUser);
+router.delete('/:userId', userController.deleteUser);
 
 module.exports = router;
